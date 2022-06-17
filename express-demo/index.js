2e9f996000b508067490c56df11d6c14fab9872c
Sweta2678 <sweta.bhavsar78@gmail.com>
2022-06-17 14:31:57 +0530

    CRUD operation with express.. validation with joi

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -26,25 +27,64 @@ app.get('/api/courses/', (req, res) => {
     res.end();
 });
 
-app.post('/api/courses/',(req,res)=>{
-    if(!req.body.name || req.body.name.length<3){
-        res.status(404).send('Name is required and should be minimum 3 character');
+app.post('/api/courses/', (req, res) => {
+
+    const {error} = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
         return;
     }
-     const course ={
-        id:courses.length+1,
-        name:req.body.name
-     };
-     courses.push(course);
-     res.send(course);
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
 
 app.get('/api/courses/:id', (req, res) => {
     //res.send(req.params.id);
+    console.log(req.params.id);
     let course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send(`Record for given Id ${id} not available.`);
+    if (!course) res.status(404).send(`Record for given Id not available.`);
     res.send(course);
 });
+
+
+app.put('/api/courses/:id', (req, res) => {
+    //lookup for courses
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    console.log(course);
+    if (!course) return res.status(404).send(`Record for given Id not available.`);
+
+    //validation
+    const {error} = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id',(req,res)=>{
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send(`Record for given Id not available.`);
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
+});
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(course);;
+}
 
 var port = process.env.PORT || 3000;
 
