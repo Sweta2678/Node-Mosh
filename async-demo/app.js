a6296c72dbd31ee9d075a931a9b8102221c64f42
Sweta2678 <sweta.bhavsar78@gmail.com>
2022-06-20 00:31:45 +0530

    named function at callback hell

--- async-demo/app.js
+++ async-demo/app.js
@@ -1,27 +1,30 @@
 console.log('before');
-getUser(1,(user)=>{
-   //console.log('User',user);
-    getRepositories(user.gitHubUsername,(repos)=>{
-        console.log('Repos',repos);
-    });
-});
+getUser(1,getRepositories);
 console.log('after');
 
-//callbacks
-//Promises
-//Async/await
+function getRepositories(user){
+    getRepositories(user.gitHubUsername,getCommits);
+}
+
+function displayCommits(commits){
+    console.log(commits);
+}
+
+function getCommits(repos){
+    console.log(repos);
+    //getCommits(repos,displayCommits);
+}
 
 function getUser(id,callback){
     setTimeout(()=>{
         console.log('Reading a user from a database...');
         callback({id: id, gitHubUsername: 'Sweta'});
-    },1000);
+    },3000);
 }
 
 function getRepositories(username,callback){
     setTimeout(()=>{
         console.log('reading repo values');
         callback(['repo1','repo2','repo3']);
-    },500);
-    //return ['repo1','repo2','repo3'];
+    },5000);
 }
