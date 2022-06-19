c2f711154dbe50d4d84aec801e21e925e8ea351b
Sweta2678 <sweta.bhavsar78@gmail.com>
2022-06-20 00:04:50 +0530

    Callback functions

--- async-demo/app.js
+++ async-demo/app.js
@@ -1,15 +1,27 @@
 console.log('before');
-const user = getUser(1);
+getUser(1,(user)=>{
+   //console.log('User',user);
+    getRepositories(user.gitHubUsername,(repos)=>{
+        console.log('Repos',repos);
+    });
+});
 console.log('after');
 
 //callbacks
 //Promises
 //Async/await
 
-function getUser(id){
+function getUser(id,callback){
     setTimeout(()=>{
         console.log('Reading a user from a database...');
-        return {id: id, gitHubUsername: 'Sweta'};
+        callback({id: id, gitHubUsername: 'Sweta'});
     },1000);
-    
+}
+
+function getRepositories(username,callback){
+    setTimeout(()=>{
+        console.log('reading repo values');
+        callback(['repo1','repo2','repo3']);
+    },500);
+    //return ['repo1','repo2','repo3'];
 }
