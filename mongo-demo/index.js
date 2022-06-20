1fff02aa1d3ac53d62345d15ea66c6ff99a9ec0f
Sweta2678 <sweta.bhavsar78@gmail.com>
2022-06-20 14:30:52 +0530

    save to db, query db

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -13,3 +13,26 @@ const courseSchema = new mongoose.Schema({
     date : { type: Date , default : Date.now},
     isPublished : Boolean
 });
+const Course  = mongoose.model('Course',courseSchema);
+
+async function createCourse(){
+    const course  = new Course({
+        name : 'Angular course',
+        author:'Mosh',
+        tags : ['angular','frontend'],
+        isPublished: true
+    });
+    const result = await course.save();
+    console.log('Result' + result);
+}
+async function getCourses(){
+    const courses = await Course
+        .find({author:'Mosh',isPublished:true})
+        .limit(10)
+        .sort({name:1})
+        .select({name:1,tags:1});
+    console.log(courses);
+}
+
+//createCourse();
+getCourses();
