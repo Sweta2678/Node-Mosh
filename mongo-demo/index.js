37c43ac7369cbcbffa98072f4d887b42a1736403
Sweta2678 <sweta.bhavsar78@gmail.com>
2022-06-20 21:25:35 +0530

    counting

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -28,7 +28,7 @@ async function createCourse(){
 async function getCourses(){
     const courses = await Course
         //comparison in query
-        //.find({author:'Mosh',isPublished:true})
+        .find({author:'Mosh',isPublished:true})
         //.find({price:{$gte  :10 }})
         //.find({price:{$gte  :10 , $lte:15}})
         //.find({price:{$in : [10,15,20]}})
@@ -46,10 +46,12 @@ async function getCourses(){
         //.find({author:'/khatsuriya$/i'}) // i for case in sensitive
 
         //contains Mosh
-        .find({author:'/.*Mosh.*/'})
+        //.find({author:'/.*Mosh.*/'})
         .limit(10)
         .sort({name:1})
-        .select({name:1,tags:1});
+        //counting
+        .count();
+       // .select({name:1,tags:1});
     console.log(courses);
 }
 
